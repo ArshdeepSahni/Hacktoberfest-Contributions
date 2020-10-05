0a4f775650e2aeffec1bf684f7909c98b914d889
Peter <peterkingsonchan@gmail.com>
2020-10-05 15:18:18 +0100

    Hacktoberfest Contribution - Todo react app

---
+++ WEB/reminders-app/src/components/Add.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+export default function Add(props) {
+    const [text, setText] = useState('')
+
+    const onFormSubmit = event => {
+        event.preventDefault()
+        props.add(text, new Date().toLocaleString())
+    }
+
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={onFormSubmit}>
+                <div className="field">
+                    <label htmlFor="add">Add reminder</label>
+                    <input type="text" id="add" value={text} onChange={e => setText(e.target.value)} />
+                </div>
+            </form>
+        </div>
+    )
+}
